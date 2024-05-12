ec7f9100352b34d9ee099c14314b7ca0c47c0c0a
sailakshmi004 <130997937+sailakshmi004@users.noreply.github.com>
2024-05-12 10:56:52 +0530

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // app.use(cors())
 app.use(cors({
-  origin: 'https://cmsproject-sailakshmi004s-projects.vercel.app'
+  origin: '*'
 }));
 
 app.get('/',async(req,res)=>
